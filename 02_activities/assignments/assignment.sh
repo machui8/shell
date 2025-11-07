5db00352bee8f8ccf046c25cae040e92b38d34a1
Ryan <machui8@users.noreply.github.com>
2025-11-07 09:02:09 -0500

    Complete

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -32,7 +32,7 @@ mkdir data
 # 2. Move the ./rawdata directory to ./data/raw
 cd data
 mkdir raw
-mv ~/dsi3/shell/02_activities/assignments/newproject/rawdata ~/dsi3/shell/02_activities/assignments/newproject/data/raw
+
 # 3. List the contents of the ./data/raw directory
 
 # 4. In ./data/processed, create the following directories: server_logs, user_logs, and event_logs
