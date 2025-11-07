3d5029771481555328c264507afc37db6cb883f2
Ryan <machui8@users.noreply.github.com>
2025-11-07 08:55:05 -0500

    Completed Part 1 Q2

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -30,7 +30,9 @@ unzip -q rawdata.zip
 # 1. Create a directory named data
 mkdir data
 # 2. Move the ./rawdata directory to ./data/raw
-
+cd data
+mkdir raw
+mv ~/dsi3/shell/02_activities/assignments/newproject/rawdata ~/dsi3/shell/02_activities/assignments/newproject/data/raw
 # 3. List the contents of the ./data/raw directory
 
 # 4. In ./data/processed, create the following directories: server_logs, user_logs, and event_logs
