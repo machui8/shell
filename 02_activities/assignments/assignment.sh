9102cc9697f9b10636fdee574d92c78268614155
Ryan <machui8@users.noreply.github.com>
2025-11-07 11:12:30 -0500

    Complete

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -30,11 +30,9 @@ unzip -q rawdata.zip
 # 1. Create a directory named data
 mkdir data
 # 2. Move the ./rawdata directory to ./data/raw
-mkdir raw
 mv ./rawdata ./data/raw
 # 3. List the contents of the ./data/raw directory
-cd ./data/raw
-ls
+
 # 4. In ./data/processed, create the following directories: server_logs, user_logs, and event_logs
 
 # 5. Copy all server log files (files with "server" in the name AND a .log extension) from ./data/raw to ./data/processed/server_logs
