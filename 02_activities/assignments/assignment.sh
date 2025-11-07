f2d58d3888e63a6744d1b83e63d6f8ed7f7ea72e
Ryan <machui8@users.noreply.github.com>
2025-11-07 10:48:50 -0500

    Complete

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -30,7 +30,7 @@ unzip -q rawdata.zip
 # 1. Create a directory named data
 mkdir data
 # 2. Move the ./rawdata directory to ./data/raw
-cd ~/dsi3/shell/02_activities/assignments/newproject/data
+cd data
 mkdir raw
 mv ~/dsi3/shell/02_activities/assignments/newproject/rawdata/ ~/dsi3/shell/02_activities/assignments/newproject/data
 # 3. List the contents of the ./data/raw directory
