5bef0a597f4b94b0c988c7f40e8aab285ce3040f
Ryan <machui8@users.noreply.github.com>
2025-11-07 13:50:11 -0500

    Complete

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -39,7 +39,8 @@ mkdir processed
 cd processed
 mkdir server_logs user_logs event_logs
 # 5. Copy all server log files (files with "server" in the name AND a .log extension) from ./data/raw to ./data/processed/server_logs
-cp ~/dsi3/shell/02_activities/assignments/newproject/data/raw/*.log ~/dsi3/shell/02_activities/assignments/newproject/data/processed/server_logs
+cd ../
+cp ./raw/server*.log ./processed/server_logs
 # 6. Repeat the above step for user logs and event logs
 
 # 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
