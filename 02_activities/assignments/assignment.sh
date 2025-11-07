3fc09a749576032a3a9739c95f1a7b20e39c8051
Ryan <machui8@users.noreply.github.com>
2025-11-07 02:01:27 -0500

    Completed Part 1 Assignment

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -37,7 +37,10 @@ mv ~/dsi3/shell/02_activities/assignments/newproject/rawdata/* ~/dsi3/shell/02_a
 cd ~/dsi3/shell/02_activities/assignments/newproject/data/raw
 ls
 # 4. In ./data/processed, create the following directories: server_logs, user_logs, and event_logs
-
+cd ~/dsi3/shell/02_activities/assignments/newproject/data
+mkdir processed
+cd processed
+mkdir server_logs user_logs event_logs
 # 5. Copy all server log files (files with "server" in the name AND a .log extension) from ./data/raw to ./data/processed/server_logs
 
 # 6. Repeat the above step for user logs and event logs
